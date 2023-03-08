d79885796204ff02c5d11c06d3f43750c05b4756
YangMunKyeong <yang4867@naver.com>
2023-03-08 22:13:50 +0900

    res = resource 즉. 필요한 정보 layout = 무엇을 보여줄지 태그를 설정함 raw = 갸공 전 파일 (ex mp3, mp4) values = 색상, 자주 사용하는 것들

---
+++ app/src/main/java/com/example/yssa_textimgageaudiovideointent/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.yssa_textimgageaudiovideointent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_gallery);
+    }
+}
